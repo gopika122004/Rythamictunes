58fa7039db7c06437901605e08d570c00b68ab7e
monikathirunavukarasu <monikathirunavukarasu@gmail.com>
2025-03-08 08:17:45 +0530

    Updates

--- vite.config.js
+++
@@ -1,8 +0,0 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-
-// https://vite.dev/config/
-export default defineConfig({
-  base: '/Rythamictunes/', // Set the base path for GitHub Pages
-  plugins: [react()],
-})
